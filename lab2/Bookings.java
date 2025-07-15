5be2f128f10bbc18a9ac5a14e95056b521d6f2e1
Rahul V B (RVB) <github.wd@rahulvb.com>
2025-07-15 14:33:06 +0530

    Lab2 Update - By sir

---
+++ lab2/Bookings.java
@@ -0,0 +1,6 @@
+// package com.demo.lab2;
+
+// public class Bookings {
+//     private String bookingDate;
+//     private Booking[] bookings;
+// }
---
+++ lab2/ZoomMeetingRoom.java
@@ -0,0 +1,6 @@
+package com.demo.lab2;
+
+public class ZoomMeetingRoom extends  MeetingRoom {
+   private String deviceId;
+   private String accountId;
+}
